c834222a6c2898872781a69ed7d3197a2de3ba0a
Kyle Kirkby <kylekirkby@googlemail.com>
2017-05-13 12:08:29 +0100

    Create circle-animation.js

---
+++ circle-animation.js
@@ -0,0 +1,36 @@
+function rotateAnnotationCropper(offsetSelector, xCoordinate, yCoordinate, cropper){
+                    //alert(offsetSelector.left);
+                
+                    var x = xCoordinate - offsetSelector.offset().left - offsetSelector.width()/2;
+                    var y = -1*(yCoordinate - offsetSelector.offset().top - offsetSelector.height()/2);
+                    var theta = Math.atan2(y,x)*(180/Math.PI);        
+
+
+                    var cssDegs = convertThetaToCssDegs(theta);
+                    var rotate = 'rotate(' +cssDegs + 'deg)';
+                    cropper.css({'-moz-transform': rotate, 'transform' : rotate, '-webkit-transform': rotate, '-ms-transform': rotate});
+                    $('body').on('mouseup', function(event){
+                    			$('body').unbind('mousemove');
+                          
+                          });
+
+            }
+            
+            function convertThetaToCssDegs(theta){
+                var cssDegs = 90 - theta;
+                return cssDegs;
+            }
+            
+            $(document).ready(function(){               
+                
+                $('#marker').on('mousedown', function(){
+                    $('body').on('mousemove', function(event){
+                        rotateAnnotationCropper($('#innerCircle').parent(), event.pageX,event.pageY, $('#marker'));    
+                    });
+                                        
+                });
+                
+                
+                $('#innerCircle').hover(function(){
+                					
+                						$('#marker').addClass('spinMarker');
